844df10688bc2b8726c42bb4d5a8fa42f39b20a8
Subhash Chandra Budde <subhashchandra.b@gmail.com>
2024-06-17 18:07:00 +0530

    Removed input prompts for daas automation
    Signed-off-by: Subhash Chandra Budde <subhashchandra.b@gmail.com>

--- db_scripts/init_db.sh
+++ db_scripts/init_db.sh
@@ -12,30 +12,23 @@ CHART_VERSION=12.0.1
 helm repo add mosip https://mosip.github.io/mosip-helm
 helm repo update
 
-# while true; do
-#     read -p "CAUTION: Do we already have Postgres installed? Also make sure the esignet DB is backed up as the same will be overriden. Do you still want to continue?" yn
-#     if [ $yn = "Y" ]
-#       then
-        DB_USER_PASSWORD=$( kubectl -n postgres get secrets db-common-secrets -o jsonpath={.data.db-dbuser-password} | base64 -d )
 
-        kubectl create ns $NS
+DB_USER_PASSWORD=$( kubectl -n postgres get secrets db-common-secrets -o jsonpath={.data.db-dbuser-password} | base64 -d )
 
-        echo Removing existing mosip_esignet DB installation
-        helm -n $NS delete postgres-init-esignet
+kubectl create ns $NS
 
-        echo Copy Postgres secrets
-        ./copy_cm_func.sh secret postgres-postgresql postgres $NS
+echo Removing existing mosip_esignet DB installation
+helm -n $NS delete postgres-init-esignet
 
-        echo Delete existing DB common sets
-        kubectl -n $NS delete secret db-common-secrets
+echo Copy Postgres secrets
+./copy_cm_func.sh secret postgres-postgresql postgres $NS
 
-        echo Initializing DB
-        helm -n $NS install postgres-init-esignet mosip/postgres-init -f init_values.yaml \
-        --version $CHART_VERSION \
-        --set dbUserPasswords.dbuserPassword="$DB_USER_PASSWORD" \
-        --wait --wait-for-jobs
-        break
-#       else
-#         break
-#     fi
-# done
+echo Delete existing DB common sets
+kubectl -n $NS delete secret db-common-secrets
+
+echo Initializing DB
+helm -n $NS install postgres-init-esignet mosip/postgres-init -f init_values.yaml \
+--version $CHART_VERSION \
+--set dbUserPasswords.dbuserPassword="$DB_USER_PASSWORD" \
+--wait --wait-for-jobs
+
